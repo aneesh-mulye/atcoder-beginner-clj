6074dce74996f30c302aff22fc3e8cd7feffeb55
Aneesh Mulye <851233+aneesh-mulye@users.noreply.github.com>
2024-08-19 03:21:44 +0530

    A few post-contest edits

--- src/atcoder/cont20240817/problems.clj
+++ src/atcoder/cont20240817/problems.clj
@@ -16,14 +16,12 @@
 (defn get-many-strings-on-n-lines [n]
   (vec (repeatedly n read-line)))
 
+
 ;; Problem 4
 (defn pedometer [n m as]
   (loop [arr0 (make-array Integer/TYPE n)
          arr1 (make-array Integer/TYPE n)
          n n]))
-
-(let [[n m] (get-many-nums-on-line)
-      as (get-many-nums-on-line)])
 
 ;; Problem 3
 (defn print-lex [acc k rs]
@@ -35,7 +33,7 @@
                  k
                  (rest rs)))))
 
-(let [[_ k] (get-many-nums-on-line)
+#_(let [[_ k] (get-many-nums-on-line)
       rs (get-many-nums-on-line)]
   (print-lex [] k rs))
 
@@ -43,13 +41,13 @@
 (defn only-long? [d]
   (= d (double (long d))))
 
-(let [d (get-double-on-line)]
+#_(let [d (get-double-on-line)]
   (println (if (only-long? d)
              (long d)
              d)))
 
 ;; Problem 1
-(let [[a b c] (get-many-nums-on-line)
+#_(let [[a b c] (get-many-nums-on-line)
       a (- a b)
       c (- c b)
       b 0]
