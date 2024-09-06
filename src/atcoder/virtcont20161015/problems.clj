769ef16fac6433ff17ab57f6cd791a4e5c4e5f12
Aneesh Mulye <851233+aneesh-mulye@users.noreply.github.com>
2024-09-07 03:50:21 +0530

    Fixed B and solved D in 20161015-046

--- src/atcoder/virtcont20161015/problems.clj
+++ src/atcoder/virtcont20161015/problems.clj
@@ -16,16 +16,37 @@
 (defn get-many-strings-on-n-lines [n]
   (vec (repeatedly n read-line)))
 
+; Problem 4
+; Extremely simple greedy strategy: get your wins as soon as ye may, take your
+; losses as you must when doing that - and that's it.
+(defn winmax [s]
+  (second 
+    (reduce
+      (fn [[slack score] c]
+        (cond
+          (and (= c \g) (zero? slack)) [1 score]
+          (and (= c \g) (pos? slack)) [(dec slack) (inc score)]
+          (and (= c \p) (zero? slack)) [1 (dec score)]
+          (and (= c \p) (pos? slack)) [(dec slack) score]))
+      [0 0]
+      s)))
+
+(println (winmax (read-line)))
+
 ; Problem 3
-; Nah, not intersted RN; maybe later, let's see.
+; Nah, not getting it RN; maybe later, let's see.
 (defn new-min-votes [a b p q]
   (cond
     (and (<= a p) (<= b q)) [p q]
     (and (<= a p) (>= b q)) []))
 
 ; Problem 2
-(let [[n k] (get-many-nums-on-line)]
-  (println (long (* k (math/pow (max 1 (dec n)) (dec k))))))
+(defn ways-of-painting [n k]
+  (cond
+    (= n 1) k
+    :else (* k (apply * (repeat (dec n) (dec k))))))
+
+(println (apply ways-of-painting (get-many-nums-on-line)))
 
 ; Problem 1
 (println (count (set (get-many-nums-on-line))))
